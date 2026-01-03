327ac96114d766dad280403e4f2e7343a255f933
ericksantos2 <ericksgbarros@gmail.com>
2026-01-03 18:47:44 -0300

    aumentei o limite, pois o app vai rodar apenas localmente mesmo

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "standalone"
+  output: "standalone",
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "2gb"
+    }
+  }
 };
 
 export default nextConfig;
